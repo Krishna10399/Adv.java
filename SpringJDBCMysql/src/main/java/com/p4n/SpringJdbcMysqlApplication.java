63288f54a8acac029a83f8a6a23f45444692e9a1
Krishna_jha1 <131844946+Krishna10399@users.noreply.github.com>
2023-12-06 09:40:38 +0530

    Add files via upload
    SPRING_JDBC_mYSQL

---
+++ SpringJDBCMysql/src/main/java/com/p4n/SpringJdbcMysqlApplication.java
@@ -0,0 +1,14 @@
+package com.p4n;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcMysqlApplication.class, args);
+	}
+
+}
